0e703e7bc860dec0f9648e3a585c5a41d53cf0c4
Chelsea Boling <cmboling@github.com>
2022-02-03 18:36:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,22 @@ function loadSomeRecords(someList, element) {
 	}
 }
 
+function loadSomeRecord(someList, element) {
+	if(someList) {
+		var someDisplayedList = document.getElementById(element);
+		var nextRecords = someDisplayedList.innerHTML;
+
+		// Alert - Declarations/UnusedVariable.ql
+		//var size = 1 + 1;
+
+		someList.forEach(item => {
+			nextRecords += `<li><a href="#" onclick="logResults('${item}');">${item}</a></li>`
+		})
+
+		someDisplayedList.innerHTML = nextRecords;
+	}
+}
+
 function logResults(name) {
   eval("console.log('This is a result: ' + name)");
 }
